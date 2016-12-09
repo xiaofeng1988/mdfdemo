5dfbd5bac59510ab67a2c2c00e3d04ac018456cd
xiaofeng <xiaofeng@192.168.31.68>
2016-12-09 18:44:25 +0800

    student实体提交

---
+++ testGit/src/testGit/Student.java
@@ -0,0 +1,14 @@
+package testGit;
+
+public class Student {
+	private String adress;
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+}
