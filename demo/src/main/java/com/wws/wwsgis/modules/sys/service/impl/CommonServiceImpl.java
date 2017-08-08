7ab9498aa622a372091164cbe14e98329ff02200
肖锋 <xiaofeng@192.168.0.10>
2017-08-08 14:43:02 +0800

    add demo by xiaofeng

---
+++ demo/src/main/java/com/wws/wwsgis/modules/sys/service/impl/CommonServiceImpl.java
@@ -0,0 +1,12 @@
+package com.wws.wwsgis.modules.sys.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wws.wwsgis.modules.sys.service.ICommonService;
+
+@Service("commonService")
+@Transactional
+public class CommonServiceImpl implements ICommonService {
+
+}
---
+++ demo/src/main/java/com/wws/wwsgis/modules/sys/service/impl/StudentServiceImpl.java
@@ -0,0 +1,75 @@
+package com.wws.wwsgis.modules.sys.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wws.wwsgis.common.utils.Paging;
+import com.wws.wwsgis.dao.StudentMapper;
+import com.wws.wwsgis.models.Student;
+import com.wws.wwsgis.modules.sys.service.IStudentService;
+
+/**
+ * 学生实现类
+ * @author 龙软科技
+ *
+ */
+@Transactional
+@Service("studentService")
+public class StudentServiceImpl implements IStudentService{
+	
+	@Resource(name="studentMapper")
+	private StudentMapper studentMapper;
+	
+
+	@Override
+	public int addStudent(Student stu) {
+		// TODO Auto-generated method stub
+		return studentMapper.insert(stu);
+	}
+
+	@Override
+	public Map<String,Object> findStudentPageByConditions(
+			Map<String, Object> paramMap) {
+		// TODO Auto-generated method stub
+		Map<String,Object> resultMap=new HashMap<String, Object>();
+		List<Map<String,Object>> resultMapList=null;
+		int total=studentMapper.findStudentCountByConditions(paramMap);
+		Paging paging=(Paging) paramMap.get("paging");
+		paging.setTotal(total);
+		if(total>0){
+			resultMapList=studentMapper.findStudentPageByConditions(paramMap);
+		}
+		resultMap.put("rows", resultMapList);
+		resultMap.put("total", total);
+		resultMap.put("paging", paging);
+		return resultMap;
+	}
+
+	@Override
+	public int updateStudent(Student stu) {
+		// TODO Auto-generated method stub
+		return studentMapper.updateByPrimaryKeySelective(stu);
+	}
+
+	@Override
+	public Student findStudentByPK(String id) {
+		// TODO Auto-generated method stub
+		return studentMapper.selectByPrimaryKey(Integer.parseInt(id));
+	}
+
+	@Override
+	public void deleteStudent(String ids) {
+		// TODO Auto-generated method stub
+		String []idsArr=ids.split(",");
+		for(int i=0;i<idsArr.length;i++){
+			studentMapper.deleteByPrimaryKey(Integer.parseInt(idsArr[i]));
+		}
+	}
+
+}
