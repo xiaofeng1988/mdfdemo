8361243ad8dccca354e83d1593909c52669d2718
肖锋 <xiaofeng@192.168.0.10>
2017-08-10 15:17:22 +0800

    admin

--- mdfdemo/target/mdfdemo-0.0.1-SNAPSHOT/static/js/chinaReport/Object.js
+++
@@ -1,14 +0,0 @@
-﻿// JScript 文件
-/*
-    //实现报表的设计
-    //Date:2007/4/1
-*/
-var str = '';  
-str+="<object  classid=\"CLSID:15261F9B-22CC-4692-9089-0C40ACBDFDD8\"";str+="CODEBASE='http://www.soft198.com/downloads/chinaexcelweb.cab#version=3,8,0,0'  name=\"ChinaExcel\" id=\"ChinaExcel\"";
-str+="style=\"left: 0px; top: 0px; width: 100%; height: 98%\">"; 
-str+="<param name=\"_Version\" value=\"65536\">";
-str+="<param name=\"_ExtentX\" value=\"20505\">";
-str+="<param name=\"_ExtentY\" value=\"14155\">";
-str+="<param name=\"_StockProps\" value=\"0\">";
-str+="</OBJECT>";
-document.write(str); 
